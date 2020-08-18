dd5c66ffe753441def2837bb386c3fd3097db2f4
mar-an <48348792+mar-an@users.noreply.github.com>
2020-08-18 11:43:19 +0200

    Revert "Revert "Update test.py""
    This reverts commit dd34aae7870102629b4f36dac8a0f7fd5eb13a94.

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 print("Hello world")
 # %%
 tab1 = [1,2,3,4,5,6]
-for iteration  in tab1:
-    print(iteration * iteration +2 /3)
+def kwadrat(x):
+    for i in x:
+        print(i*i)
+kwadrat(tab1)    
 # %%
