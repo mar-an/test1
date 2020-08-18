d2684fbcc7d93b3ad6510bd3b0687f539e75d3ed
mar-an <48348792+mar-an@users.noreply.github.com>
2020-08-18 18:25:23 +0200

    poprawka5

--- funClass.py
+++ funClass.py
@@ -1,15 +1,21 @@
 class function:
+    
     def __init__(self, x, y):
         self.x=x
         self.y=y
+        self.newList = []
+      
 
     def kwadrat(self):
         for i in self.x:
             for j in self.y:
                 if j == i:
+                    self.newList.append(j)
                     print(j)
 #               #else:
 #               #    print("brak powiązania dla" +str(j))
                 
     def showme(self): 
-        print("jestem w showme")
+        for (i, item) in enumerate(self.newList, start=1):
+            print(i, item)
+      
