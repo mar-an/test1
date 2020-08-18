d87233ce4c5a0e6401f8ec3d2aa5deb56f19ad74
mar-an <48348792+mar-an@users.noreply.github.com>
2020-08-18 13:26:32 +0200

    nowy plik klasy

---
+++ funClass.py
@@ -0,0 +1,8 @@
+class function:
+    #def kwadrat(x,y):
+    for i in x:
+        for j in y:
+            if j == i:
+                print(j)
+            #else:
+                #print("brak powiązania dla" +str(j))
--- test.py
+++ test.py
@@ -1,9 +1,8 @@
+#from funClass import function
 #%%
 print("Hello world")
 # %%
 tab1 = [1,2,3,4,5,6]
-def kwadrat(x):
-    for i in x:
-        print(i*i)
-kwadrat(tab1)    
+tab2 = [1,3,5,7,9,11]
+kwadrat(tab1, tab2)    
 # %%
