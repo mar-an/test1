4d40a385c60e710bd7f502d33a35e20c939915f2
mar-an <48348792+mar-an@users.noreply.github.com>
2020-08-19 13:59:18 +0200

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	test.py

--- trainingClass.py
+++ trainingClass.py
@@ -1,5 +1,12 @@
 class MyNum(object):
+
+# init which do preperation work like check if file is correct format like CSV or XML
     def __init__(self, value):
+        try:
+            value = int(value)
+        except ValueError:
+            print ("don't use NOT string value like " +value)
+            exit(1)
         self.val = value
 
     def increment(self):
